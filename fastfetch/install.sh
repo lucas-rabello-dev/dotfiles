3c621078298036d2e06ad03080ed0768cb4f94cb
lucas-rabello-dev <lucasrabello2020@gmail.com>
2026-05-01 15:14:46 -0300

    add: fastfetch/

---
+++ fastfetch/install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# -d (verifica se o diretorio existe)
+# [] shell test, avalia se verdadeiro ou falso
+# -f (verifica se tem um arquivo em comum)
+# -r pra copiar o diretorio 'logo/' todo
+# -p (se nao existir ele cria, se existir ele nao reclama..)
+
+DIR="$HOME/.config/fastfetch"
+
+mkdir -p "$DIR"
+
+if [ -f "$DIR/config.jsonc" ]; then
+    mv "$DIR/config.jsonc" "$DIR/config_old.jsonc"
+fi
+
+cp -r logo/ "$DIR/"
+cp ./config.jsonc "$DIR/"
